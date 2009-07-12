9a24bffe03f1d27c861eaa4a74e48cfd7eb611ff
Alexander Kerner <lunohod@openinkpot.org>
2009-07-13 01:18:34 +0200

    gui redesign

--- src/elock.c
+++ src/elock.c
@@ -39,8 +39,6 @@
 #define LOCK "Lock"
 #define UNLOCK "Unlock"
 
-#define NOTIME "--:--"
-
 Ecore_Evas *main_win;
 
 void exit_all(void* param) { ecore_main_loop_quit(); }
@@ -52,63 +50,6 @@ static void die(const char* fmt, ...)
 	vfprintf(stderr, fmt, ap);
 	va_end(ap);
 	exit(EXIT_FAILURE);
-}
-
-static void update_time()
-{
-	Evas_Object *o = evas_object_name_find(ecore_evas_get(main_win), "edje");
-	char *tstr = NULL;
-	time_t tim = time(NULL);
-	struct tm *t = localtime(&tim);
-
-	if(t->tm_year >= 109) {
-		asprintf(&tstr, "%2d:%02d", t->tm_hour, t->tm_min);
-		edje_object_part_text_set(o, "elock/time", tstr);
-		free(tstr);
-	} else
-		edje_object_part_text_set(o, "elock/time", NOTIME);
-}
-
-static void update_battery()
-{
-    char b[10];
-    int charge;
-    int x;
-    FILE *f_cf, *f_cn;
-
-    if((f_cn = fopen("/sys/class/power_supply/n516-battery/charge_now", "r")) != NULL)
-		f_cf = fopen("/sys/class/power_supply/n516-battery/charge_full_design", "r");
-	else {
-		f_cn = fopen("/sys/class/power_supply/lbookv3_battery/charge_now", "r");
-		f_cf = fopen("/sys/class/power_supply/lbookv3_battery/charge_full_design", "r");
-	}
-
-    if((f_cn != NULL) && (f_cf != NULL)) {
-        fgets(b, 10, f_cn);
-        charge = atoi(b);
-        fgets(b, 10, f_cf);
-        x = atoi(b);
-        if(x > 0)
-            charge = charge * 100 / atoi(b);
-    } else
-        charge = 0;
-
-    if(f_cn != NULL)
-        fclose(f_cn);
-    if(f_cf != NULL)
-        fclose(f_cf);
-
-	Evas_Object *o = evas_object_name_find(ecore_evas_get(main_win), "edje");
-	if(charge < 5)
-		edje_object_signal_emit(o, "set_batt_empty", "");
-	else if(charge < 25)
-		edje_object_signal_emit(o, "set_batt_1/4", "");
-	else if(charge < 50)
-		edje_object_signal_emit(o, "set_batt_2/4", "");
-	else if(charge < 75)
-		edje_object_signal_emit(o, "set_batt_3/4", "");
-	else
-		edje_object_signal_emit(o, "set_batt_full", "");
 }
 
 typedef struct
@@ -134,8 +75,6 @@ static int _client_del(void* param, int ev_type, void* ev)
 
     /* Handle */
 	if(strlen(LOCK) == msg->size && !strncmp(LOCK, msg->msg, msg->size)) {
-		update_time();
-		update_battery();
 		ecore_evas_show(main_win);
 	} else if(strlen(UNLOCK) == msg->size && !strncmp(UNLOCK, msg->msg, msg->size))
 		ecore_evas_hide(main_win);
@@ -182,8 +121,7 @@ int main(int argc, char **argv)
 		die("Unable to initialize Edje\n");
 
 	setlocale(LC_ALL, "");
-	textdomain("elock");
-
+	textdomain("elock"); 
 	ecore_con_server_add(ECORE_CON_LOCAL_USER, "elock", 0, NULL);
 
 	ecore_event_handler_add(ECORE_CON_EVENT_CLIENT_ADD, _client_add, NULL);
@@ -210,13 +148,10 @@ int main(int argc, char **argv)
 	evas_object_resize(edje, 600, 800);
 	evas_object_show(edje);
 
-	update_time();
-	update_battery();
+	edje_object_part_text_set(edje, "elock/title", gettext("Keyboard Lock"));
+	edje_object_part_text_set(edje, "elock/text", gettext("Press and hold \"OK\" for 3-4 seconds to unlock the device"));
 
-	char *t1 = gettext("Press and hold \"OK\" for 3-4 seconds to unlock the device");
-	edje_object_part_text_set(edje, "elock/label", t1);
-
-//	ecore_evas_show(main_win);
+	ecore_evas_show(main_win);
 	ecore_main_loop_begin();
 
 	edje_shutdown();
